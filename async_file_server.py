4a0afc009baf56c3b35a0982569ef41cf6827806
yoshi-lyosha <atanov_wow@mail.ru>
2017-07-29 18:31:48 +0300

    Фикс отдачи файлов. Сделал как статику, теперь можно отдавать не только текст.

--- async_file_server.py
+++ async_file_server.py
@@ -21,19 +21,6 @@ def parse_ip_port(server):
     return ip, port
 
 
-class GetFileHandler:
-    def __init__(self, project_path):
-        self.path = project_path
-
-    async def handle(self, request):
-        file_name = request.match_info.get('file_name', 'None')
-        if os.path.exists('{}/{}'.format(self.path, file_name)):
-            with open('{}/{}'.format(self.path, file_name), 'r') as file:
-                return web.Response(text=file.read())
-        else:
-            return web.HTTPInternalServerError()
-
-
 if __name__ == '__main__':
     parser_args = parse_args()
     server_ip, server_port = parse_ip_port(parser_args.server)
@@ -42,10 +29,6 @@ if __name__ == '__main__':
         sys.exit(errno.EINVAL)
 
     project_root = os.path.abspath(os.path.curdir)
-
     app = web.Application()
-
-    handler = GetFileHandler(project_root)
-    app.router.add_get('/get/{file_name}', handler.handle)
-
+    app.router.add_static('/get/', path=project_root)
     web.run_app(app, host=server_ip, port=server_port)
